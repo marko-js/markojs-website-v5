772617e81a8f3fbfb3dadbb80c4ac32d6689e7f4
dpiercey <dpiercey@ebay.com>
2025-01-06 14:14:29 -0700

    fix: shake256 browser shim

--- browser-shims/compiler/index.js
+++ browser-shims/compiler/index.js
@@ -1,2 +1,26 @@
 // We don't bring in the full Marko compiler, but the ast types are needed.
 exports.types = require("./dist/babel-types");
+
+const crypto = require("crypto");
+const createHash = crypto.createHash;
+
+if (!crypto.getHashes().includes("shake256")) {
+  crypto.createHash = (alg, opts) => {
+    if (alg !== "shake256") return createHash(alg, opts);
+
+    const hash = createHash("md5", opts);
+    return {
+      update(val) {
+        hash.update(val);
+        return this;
+      },
+      digest(enc) {
+        if (opts.outputLength) {
+          return hash.digest(enc).slice(0, opts.outputLength)
+        }
+
+        return hash.digest(enc);
+      }
+    }
+  }
+}
